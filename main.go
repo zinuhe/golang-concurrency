23e9bbafe4f487a695a7ddf993b3e37a99baa358
Jimmy <zinuhe@outlook.com>
2021-11-02 00:18:32 -0400

    Update main.go

--- main.go
+++ main.go
@@ -9,22 +9,6 @@ func main (){
     fmt.Println("Concurrency")
     // https://www.youtube.com/watch?v=yNOe3STbtGE
 
-    //Simple one
-    // out1 :=  make(chan string)
-    // go process("order", out1)
-    // for msg := range out1 {
-    // 	fmt.Println(msg)
-    // }
-
-    // func process(item string, out chan string) {
-    //     defer close(out)
-    //     for i:=0; i< 5; i++ {
-    //         time.Sleep(time.Second / 2)
-    //         out <- item
-    //     }
-    // }
-
-    //Not that simple
     out1 :=  make(chan string)
     out2 :=  make(chan string)
 
